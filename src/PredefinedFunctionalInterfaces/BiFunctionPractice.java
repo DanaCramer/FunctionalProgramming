628e0452c2d1865b1d3d481a24d99772fe5d1e69
Dana <dana_cramer@web.de>
2024-05-24 20:12:43 +0200

    practice BiFunction and BinaryOperator

---
+++ src/PredefinedFunctionalInterfaces/BiFunctionPractice.java
@@ -0,0 +1,13 @@
+package PredefinedFunctionalInterfaces;
+
+import java.util.function.BiFunction;
+
+public class BiFunctionPractice {
+
+    public static void main(String[] args) {
+
+        BiFunction<String, String, Integer> biFunction = (a, b) -> (a + b).length();
+
+        System.out.println(biFunction.apply("Basics", "Strong"));
+    }
+}
---
+++ src/PredefinedFunctionalInterfaces/BinaryOperatorPractice.java
@@ -0,0 +1,13 @@
+package PredefinedFunctionalInterfaces;
+
+import java.util.function.BinaryOperator;
+
+public class BinaryOperatorPractice {
+
+    public static void main(String[] args) {
+
+        BinaryOperator<String> operator = (a, b) -> a + "." + b;
+
+        System.out.println(operator.apply("BasicsStrong", "com"));
+    }
+}

0b64c68cace5a83c070b36729870741bd4ed212f
Dana <dana_cramer@web.de>
2024-05-28 22:12:59 +0200

    practice method references

---
+++ src/References/StaticReference.java
@@ -0,0 +1,20 @@
+package References;
+
+import java.util.function.BiFunction;
+
+public class StaticReference {
+
+    public static void main(String[] args) {
+
+        BiFunction<String, String, String> biFunction = A_Class :: staticMethod;
+
+        System.out.println(biFunction.apply("Basics","Strong"));
+    }
+}
+
+class A_Class{
+
+    static String staticMethod(String a, String b) {
+        return a + b;
+    }
+}
---
+++ src/References/SupplierPractice.java
@@ -0,0 +1,17 @@
+package References;
+
+import java.util.function.Supplier;
+
+public class SupplierPractice {
+
+    public static void main(String[] args) {
+
+        Supplier<String> stringSupplier = () -> new String("A String");
+
+        System.out.println("stringSupplier = " + stringSupplier.get());
+
+        Supplier<Double> randomNumber = Math :: random;
+
+        System.out.println("randomNumber = " + randomNumber.get());
+    }
+}

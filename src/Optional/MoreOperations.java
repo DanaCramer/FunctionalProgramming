3e43320f24e1fbfa08c08da10a5683f27b7ff502
Dana <dana_cramer@web.de>
2024-06-25 18:56:08 +0200

    Optional operations

---
+++ src/Optional/MoreOperations.java
@@ -0,0 +1,36 @@
+package Optional;
+
+import javax.net.ssl.SSLContext;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+public class MoreOperations {
+
+    public static void main(String[] args) {
+
+        //ifPresent
+        Optional<String> optional = Optional.of("Value");
+        optional.ifPresent(val -> System.out.println("This is a " + val));
+
+        //ifPresentOrElse
+        Optional.empty().ifPresentOrElse(System.out :: println, () -> System.out.println("Value is absent"));
+
+        //stream
+        Stream<String> stream = optional.stream();
+        Optional.empty().stream().forEach(System.out :: println);
+
+        //or
+        optional.or(() -> Optional.of("New Value")).ifPresent(System.out::println);
+        Optional.empty().or(() -> Optional.of("New Value")).ifPresent(System.out :: println);
+        //Optional.empty().or(() -> null).ifPresent(System.out :: println);
+
+        //equals
+        //optional
+        //either both are empty
+        //or if the values in optionals are equal to each other via equal method
+        System.out.println(optional.equals(Optional.of("Value")));
+
+        //hashcode
+        System.out.println(optional.hashCode());
+    }
+}

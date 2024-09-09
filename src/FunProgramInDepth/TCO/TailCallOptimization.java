15d6ecca3ddc6281ad9f081f6f16fbc2e6552d54
Dana <dana_cramer@web.de>
2024-09-09 16:34:45 +0200

    Currying + TCO

---
+++ src/FunProgramInDepth/TCO/TailCallOptimization.java
@@ -0,0 +1,23 @@
+package FunProgramInDepth.TCO;
+
+public class TailCallOptimization {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static long reFact(int n) {
+        if (n <= 1)
+            return 1;
+        else
+            return n * reFact(n - 1);
+    }
+
+    public static long tailReFact(int n, int a) {
+
+        if (n <= 1)
+            return a;
+        else
+            return tailReFact(n - 1, n * a);
+    }
+}

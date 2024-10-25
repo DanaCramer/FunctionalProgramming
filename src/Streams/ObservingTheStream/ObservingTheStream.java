862ff0d54dea07a34c49e4aec225ae4c8517846d
Dana <dana_cramer@web.de>
2024-10-25 19:12:34 +0200

    Observing the Stream

--- src/Streams/ObservingTheStream/ObservingTheStream.java
+++ src/Streams/ObservingTheStream/ObservingTheStream.java
@@ -11,21 +11,22 @@ public class ObservingTheStream {
     public static void main(String[] args) {
         List<Book> books = new ArrayList<>();
 
+        //Stream Pipeline
         List<Book> popularyBooks = books.stream()
-                .filter((book) -> book.getGenre().equalsIgnoreCase("Horror"))
-                .filter((book) -> book.getRating() > 3)
-                .toList();
+                .filter((book) -> book.getGenre().equalsIgnoreCase("Horror")) //Intermediate Operation
+                .filter((book) -> book.getRating() > 3) //Intermediate Operation
+                .toList(); //Terminal Operation
 
-        //1.
+        //1. Source
         Stream<Book> stream = books.stream();
 
-        //2.
+        //2. Intermediate Operation
         Stream<Book> horrorBooks = stream.filter((book) -> book.getGenre().equalsIgnoreCase("Horror"));
 
-        //3.
+        //3. Intermediate Operation
         Stream<Book> popularyHorrorBooks = horrorBooks.filter((book) -> book.getRating() > 3);
 
-        //4.
+        //4. Terminal Operation
         List<Book> collect = popularyHorrorBooks.toList();
     }
 }
